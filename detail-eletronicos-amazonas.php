ee2dd60f7a5988045da87ba98259dcad2b5c9f34
MarcosTresco <jomimasaude@gmail.com>
2025-08-19 10:11:56 -0300

    Create detail-eletronicos-amazonas.php

---
+++ detail-eletronicos-amazonas.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Detalhes Amazonas Vestuário e Artigos Infantis</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- Favicon -->
+    <link href="img/icon.png" rel="icon">
+
+    <!-- Google Web Fonts -->
+	<script src="https://kit.fontawesome.com/dbd4da7a61.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+	<link href="css/mystyle.css" rel="stylesheet">
+	
+	
+	<style>
+		.avaliacao{display:flex}
+		.star-icon{
+			list-style-type:none;
+			border:1px solid #fff;
+			cursor:pointer;
+			color:#a3cb4c;
+			font-size:40px;	
+		}
+		
+		.star-icon::before{
+			content:"\2605";			
+		}
+		
+		.star-icon.ativo ~ .star-icon::before{
+			content:"\2606";			
+		}
+		.avaliacao:hover .star-icon::before{
+			content:"\2605";
+		}
+		.star-icon:hover ~ .star-icon::before{
+			content:"\2606";
+		}	
+
+	</style>
+</head>
+
+
+<body>
+    <!-- Topbar Start -->
+   <?php include "menu.php";?>
+    <!-- Navbar End -->
+
+
+    <!-- Breadcrumb Start -->
+    <div class="container-fluid">
+        <div class="row px-xl-5">
+            <div class="col-12">
+                <nav class="breadcrumb bg-light mb-30">
+                    <a class="breadcrumb-item text-dark" href="index.php">Inicio</a>
+                    <a class="breadcrumb-item text-dark" href="sites.php">Sites</a>
+                    <span class="breadcrumb-item active">Detalhe do Produto</span>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb End -->
+
+
+    <!-- Shop Detail Start -->
+    <div class="container-fluid pb-5">
+        <div class="row px-xl-5">
+            <div class="col-lg-5 mb-30">
+                <div id="product-carousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner bg-light">
+                        <div class="carousel-item active">
+                            <img class="w-100 h-100" src="nuvem-img/amazonas-infantil/amazonas-infantil.jpg" alt="Imagem do layout da loja virtual ecommerce">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100 h-100" src="nuvem-img/amazonas-infantil/amazonas-infantil2.jpg" alt="Imagem do layout da loja virtual ecommerce">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100 h-100" src="nuvem-img/amazonas-infantil/amazonas-infantil-mobile.jpg" alt="Imagem do layout da loja virtual ecommerce">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
+                        <i class="fa fa-2x fa-angle-left text-dark"></i>
+                    </a>
+                    <a class="carousel-control-next" href="#product-carousel" data-slide="next">
+                        <i class="fa fa-2x fa-angle-right text-dark"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-lg-7 h-auto mb-30">
+                <div class="h-100 bg-light p-30">
+					<small class="pt-1">SKU. JM0007</small>				
+                    <h3 class="text-primaryjj" >Amazonas - Artigos e Vestuário Infantil</h3>
+					<h6 class="text-primaryp text-muted ml-2"><del>De R$ 3499,00</del></h6>					
+                    <h3 class="text-primaryjj font-weight-semi-bold mb-4">Por R$ 2599,00</h3>
+					
+					<div class="d-flex align-items-center mb-4 pt-2">                       
+                       <a href="https://wa.me/5544991185953" target="blank"> <button class="text-dark btn btn-primary px-3"><i class="fa fa-shopping-cart mr-1"></i> Finalizar Compra</button></a>
+                    </div>
+					
+                    <p class="mb-4">Maximize sua presença online e alcance um público maior com um site responsivo e profissionalmente desenvolvido para representar sua marca. </p>
+
+					<small class="pt-1">(03 Vendidos)</small>	
+                </div>
+            </div>
+        </div>
+        <div class="row px-xl-5">
+            <div class="col">
+                <div class="bg-light p-30">
+                    <div class="nav nav-tabs mb-4">
+                        <a class="nav-item nav-link text-dark active" data-toggle="tab" href="#tab-pane-1">Descrição</a>
+                        <a class="nav-item nav-link text-dark" data-toggle="tab" href="#tab-pane-2">Informação</a>
+                        <a class="nav-item nav-link text-dark" data-toggle="tab" href="#tab-pane-3">Avaliação (0)</a>
+                    </div>
+                    <div class="tab-content">
+                        <div class="tab-pane fade show active" id="tab-pane-1">
+                            <h4 class="mb-3">Descrição do Produto</h4>
+							<h4>Serviços</h4> 
+
+							<p>Desenvolvimento de loja virtual</p>
+						<ul>
+							<li>Desenvolvimento de layout das páginas que compõem a loja virtual até 48 produtos*</li>
+							<li>Desenvolvimento dos banners principais e de categorias**</li>
+							<li>Registro de domínio DNS por 1 ano</li>
+							<li>Construção agradável para Google Search (SEO) </li>
+							<li>Botão de WhatsApp (Opcional) </li>
+							<li>Páginas essenciais (Sobre, Política de Privacidade, Trocas e Devoluções) </li>
+							<li>Cadastro de sistema de pagamento</li>
+							<li>Cadastro de sistema de entrega</li>
+							<li>Painel de controle administrativo da loja virtual (dashboard) </li>
+							<li>Gerenciamento de clientes</li>
+							<li>Gerenciamento de pedidos</li>
+							<li>Informações financeiras da loja virtual</li>
+							<li>Leads</li>
+							<li>Cadastro e edição de produtos</li>
+							<li>Painel de controle Macro da loja virtual</li>
+						</ul>
+
+							<h4>Produtos</h4> 
+						<ul>
+							<li>Loja Entregue completa e funcional</li>
+							<li>Manual em PDF ou Vídeo de utilização do painel de controle
+						<ul>
+
+							<li>Como cadastrar novos produtos</li>
+							<li>Como editar um produto existente</li>
+							<li>Como excluir um produto</li>
+							<li>Como criar uma nova categoria</li>
+							<li>Como editar uma categoria existente</li>
+							<li>Como excluir uma categoria</li>
+							<li>Como criar um pedido manual dentro do seu site</li>
+						</ul>
+						<br>
+
+						<p class="nav-item nav-link text-dark active" data-toggle="tab" href="#tab-pane-1">*48 são o número máximo de produtos cadastrados para este site, para um portifólio mais abrangente sugerimos que entre em contato para aquisição de um projeto pensado especificamente para o seu negócio.</p>
+						<p class="nav-item nav-link text-dark active" data-toggle="tab" href="#tab-pane-1">**Adquirindo esse produto forneceremos todas as artes conceituais que se fizerem necessárias para o layout do projeto. No entanto é necessário que você cliente que o adquiriu forneça informações sobre sua marca, bem como artes em boa resolução e descritivos e informações técnicas do seu produto.</p>
+						<br>
+						<strong class="text-dark mr-2">Posterior ao pagamento entraremos em contato para celebração de um contrato de prestação de serviço, visando a garantia dos serviços prestados, bem como a segurança de dados e informações pessoais do Cliente.</strong>						
+						</div>
+                        <div class="tab-pane fade" id="tab-pane-2">
+                            <h4 class="mb-3">Informações Adicionais</h4>
+							<p>Artigos e Vestuário Infantil é uma loja virtual online que permite que empreendedores vendam seus produtos e serviços pela internet. Com o produto, é possível gerenciar estoques, preços, pedidos e entregas, além de oferecer uma experiência de compra personalizada e segura para os clientes. Dessa forma, é possível expandir os negócios e alcançar uma audiência global, sem as limitações geográficas e horárias das lojas físicas tradicionais.</p>
+							<p>Preparamos tudo e entregamos sua loja pronta para você apenas vender e fazer a gestão dos produtos.</p>
+							<p>Montagem completa e personalização exclusiva da sua loja virtual (e-commerce) na Nuvem Shop, ou seja, garantia de qualidade e segurança para você e seus clientes.</p>
+							<ul>
+								<li>Até 48 produtos cadastrados</li>
+								<li>Imagens dos produtos devem ser enviadas pelo cliente</li>
+							</ul>
+						   <div class="row">
+                                <div class="col-md-6">
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item px-0">
+                                            Domínio: o endereço da web que identifica o site grátis por 1 ano.
+                                        </li>
+                                        <li class="list-group-item px-0">
+                                            Hospedagem: o espaço de armazenamento online que hospeda os arquivos do site deve ser pago mensalmente.
+                                        </li>
+                                        <li class="list-group-item px-0">
+                                            Design: a aparência e a funcionalidade do site, incluindo layout, cores, fontes e elementos gráficos.
+                                        </li>
+                                        <li class="list-group-item px-0">
+                                            Conteúdo: o texto, imagens e outros elementos que fornecem informações e interação para os visitantes do site.
+                                        </li>
+                                      </ul> 
+                                </div>
+                                <div class="col-md-6">
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item px-0">
+                                            Funcionalidades: recursos avançados, como formulários de contato, sistemas de gerenciamento de conteúdo, carrinho de compras, entre outros.
+                                        </li>
+                                        <li class="list-group-item px-0">
+                                            SEO: otimização para mecanismos de busca, para melhorar o posicionamento do site nos resultados de pesquisa.
+                                        </li>
+                                        <li class="list-group-item px-0">
+                                            Responsividade: capacidade de se adaptar a diferentes dispositivos e tamanhos de tela, para proporcionar uma experiência de navegação confortável em qualquer dispositivo.
+                                        </li>
+                                        <li class="list-group-item px-0">
+                                            Segurança: proteção contra ataques cibernéticos e vazamento de informações pessoais do usuário.
+                                        </li>
+                                      </ul> 
+                                </div>
+                            </div>
+							<h4>Porque comprar uma loja virtual?</h4>
+							<p>Menor investimento inicial: Uma loja virtual pode ser criada com um investimento inicial muito menor do que uma loja física. Além disso, muitos dos custos associados a uma loja física, como aluguel, contas de luz e água, não se aplicam a uma loja virtual. </p>
+							<p>Maior alcance geográfico: Uma loja virtual pode ser acessada por qualquer pessoa com acesso à internet, permitindo que você alcance uma audiência global. Isso pode significar maiores vendas e lucros para o seu negócio.
+							<p>Maior flexibilidade: Uma loja virtual permite que você trabalhe de qualquer lugar com acesso à internet, o que pode ser particularmente útil se você precisar viajar ou se mudar para outra cidade. </p>
+							<p>Maior facilidade de gerenciamento: Com as ferramentas certas, gerenciar uma loja virtual pode ser mais fácil do que gerenciar uma loja física. Por exemplo, você pode usar softwares de automação de marketing para gerenciar suas campanhas de publicidade online e plataformas de e-commerce para gerenciar seus pedidos e estoque. </p>
+							<p>Maior capacidade de coletar dados: Uma loja virtual pode ser integrada a ferramentas de análise de dados, permitindo que você colete informações valiosas sobre seus clientes e seu comportamento de compra. Essas informações podem ser usadas para otimizar suas campanhas de marketing e melhorar suas estratégias de vendas. </p>
+							<p>Em resumo, comprar uma loja virtual pode ser uma ótima opção para quem deseja iniciar um negócio com menor investimento inicial, maior alcance geográfico e maior flexibilidade de trabalho. Além disso, gerenciar uma loja virtual pode ser mais fácil e permitir que você colete informações valiosas sobre seus clientes e seu negócio. </p>
+
+                        </div>
+                        <div class="tab-pane fade" id="tab-pane-3">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h4 class="mb-4">Nenhuma Avaliação</h4>
+                                    <div class="media mb-4">
+                                       <!-- <img src="img/user.jpg" alt="Image- fota da pessoa que fez o comentário" class="img-fluid mr-3 mt-1" style="width: 45px;">
+                                        <div class="media-body">
+                                            <h6>John Doe<small> - <i>01 Jan 2045</i></small></h6>
+                                            <div class="text-primaryj mb-2">
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star-half-alt"></i>
+                                                <i class="far fa-star"></i>
+                                            </div>
+                                            <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.</p>
+                                        </div>-->
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <h4 class="mb-4">Faça sua Avaliação</h4>
+                                    <small>Seu E-mail não aparecerá, os campos obrigatórios são marcados com *</small>
+                                    <div class="d-flex my-3">
+                                        <p class="mb-0 mr-2">Sua Nota :</p>
+                                        <div class="text-primaryj">
+                                           <i class="star-icon ativo" data-avaliacao="1" ></i>
+                                           <i class="star-icon" data-avaliacao="2" ></i>
+                                           <i class="star-icon" data-avaliacao="3" ></i>
+                                           <i class="star-icon" data-avaliacao="4" ></i>
+                                           <i class="star-icon" data-avaliacao="5" ></i>
+                                        </div>
+                                    </div>
+                                    <form action="avaliacao.php" method="POST" id="avalia">
+                                        <div class="form-group">
+                                            <label for="message">Sua Avaliação*</label>
+                                            <textarea name="avaliacao" id="avaliacao" cols="30" rows="5" class="form-control" required></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="name">Seu Nome *</label>
+                                            <input type="text" name="nome" class="form-control" id="nome" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="email">Seu E-mail *</label>
+                                            <input type="email" name="email" class="form-control" id="email" required>
+                                        </div>
+                                        <div class="form-group mb-0">
+                                            <input type="submit" value="Enviar sua Avaliação" class="btn btn-primary px-3">
+                                        </div>
+										<br>
+										<p>Por favor, respeite as opiniões e pontos de vista dos outros e evite comentários ofensivos ou prejudiciais. Nossa equipe fará analise da mensagem e caso esteja de acordo com nossas regras ele será publicado.</p>
+                                    </form> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Shop Detail End -->
+
+
+    <!-- Products Start -->
+		<?php include "include-carrocel-de-produtos.php";?>
+    <!-- Products End -->
+
+
+    <!-- Footer Start -->
+		<?php include "footer.php";?>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+	
+	<script>
+var stars = document.querySelectorAll('.star-icon');
+document.addEventListener('click',function(e){
+	var classStar = e.target.classList;
+	if(!classStar.contains('ativo')){
+	stars.forEach(function(star){
+	star.classList.remove('ativo');
+	});
+	classStar.add('ativo');
+	console.log(e.target.getAttribute('data-avaliacao'))
+	}
+});
+</script>
+</body>
+
+</html>
